6d6cc8a6274164f1126f63438e58d364fd61dca0
Michael Pankraz <>
2021-05-26 07:05:56 +0200

    Add todos

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ from src.template_builder import TemplateBuilder
 workingDirectory = os.getcwd()
 configPath = workingDirectory + '/template_builder_config.json'
 TemplateBuilder.run(sys.argv, configPath)
+
+# TODOs:
+# - add writeFile? True/False to content properties
+# - add overrideExistingFile? True/False to content properties
+# - replace variable : separator with ?
